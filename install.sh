5ec7d1145c1076d9fad985b81a4bfc6760e9ef4d
Rafi Halilintar <masitingss@gmail.com>
2018-12-01 14:14:53 +0700

    First commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+# GitHub:   https://github.com/masitings/Lemposite
+# Author:   Thamaraiselvam
+# URL:      https://rafi-halilintar.me
+#
+
+clear
+printf "
+#######################################################################
+#       OneinStack for CentOS/RedHat 6+ Debian 7+ and Ubuntu 12+      #
+#       For more information please visit https://oneinstack.com      #
+#######################################################################
+"
