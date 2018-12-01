a0fa53b03819e7009480d905452219cb1aaec00e
Rafi Halilintar <masitingss@gmail.com>
2018-12-01 14:44:58 +0700

    Kedua

--- install.sh
+++ install.sh
@@ -17,6 +17,9 @@ printf "
 dbrootpwd=`< /dev/urandom tr -dc A-Za-z0-9 | head -c8`
 # Melakukan validasi apakah root atau bukan yang login dan akan mengeksekusi script ini.
 [ $(id -u) != "0" ] && { echo "${CFAILURE}Error: Maaf, script ini hanya bisa dijalankan oleh super user (root)${CEND}"; exit 1; }
+
+lemposite_dir=$(dirname "`readlink -f $0`")
+
 printf "
 Aplikasi yang akan di install : 
     [*] Nginx Web Server
@@ -29,6 +32,7 @@ if [[ ! ${installasi} =~ ^[y,n]$ ]]; then
 fi
 case "${installasi}" in
     y )
-    echo "Install nginx"
+    . include/nginx.sh
+    Install_Nginx 2>&1 | tee -a ${oneinstack_dir}/install.log
         ;;
 esac
