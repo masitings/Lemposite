ba6b47bf75a7630ecee282ebba48f9f9f79bfbf6
Rafi Halilintar <masitingss@gmail.com>
2018-12-01 14:21:40 +0700

    Kedua

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+# GitHub:   https://github.com/masitings/Lemposite
+# Author:   Thamaraiselvam
+# URL:      https://rafi-halilintar.me
+#
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
+clear
+printf "
+#######################################################################
+#                          LEMPOSITE WEBSERBER                        #
+#     For more information please visit https://rafi-halilintar.me    #
+#######################################################################
+"
