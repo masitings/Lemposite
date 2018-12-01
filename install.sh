88ac48bf18268b6a5b5f889e41dc18c42a4e7bf9
Rafi Halilintar <masitingss@gmail.com>
2018-12-01 14:27:33 +0700

    Kedua

--- install.sh
+++ install.sh
@@ -10,6 +10,15 @@ clear
 printf "
 #######################################################################
 #                          LEMPOSITE WEBSERBER                        #
-#     For more information please visit https://rafi-halilintar.me    #
+#        Custom installation script https://rafi-halilintar.me        #
+#       Aplikasi ini hanya berlaku untuk Linux (Debian & Ubuntu)      #
 #######################################################################
+"
+# Melakukan validasi apakah root atau bukan yang login dan akan mengeksekusi script ini.
+[ $(id -u) != "0" ] && { echo "${CFAILURE}Error: Maaf, script ini hanya bisa dijalankan oleh super user (root)${CEND}"; exit 1; }
+printf "
+Aplikasi yang akan di install : 
+    [*] Nginx Web Server
+    [*] MariaDB
+    [*] PHP 7.2
 "
